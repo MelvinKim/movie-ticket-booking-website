509d82c954741258d7d387813aa95b283fce1b11
Melvin Kimathi <53833444+MelvinKim@users.noreply.github.com>
2021-08-15 22:53:53 +0300

    Initial commit

---
+++ src/main/java/com/melvinkimathi/springbootprojects/movieticketsbooking/MovieTicketsBookingApplication.java
@@ -0,0 +1,13 @@
+package com.melvinkimathi.springbootprojects.movieticketsbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketsBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTicketsBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melvinkimathi/springbootprojects/movieticketsbooking/MovieTicketsBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melvinkimathi.springbootprojects.movieticketsbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketsBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
